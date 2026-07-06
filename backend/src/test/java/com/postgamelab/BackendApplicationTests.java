87d1152e933866f39977f584fc5fb6249fd93c89
Varun Choubey <varunofficial21@gmail.com>
2026-07-06 17:19:52 +0530

    Create Spring Boot backend project

---
+++ backend/src/test/java/com/postgamelab/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postgamelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
